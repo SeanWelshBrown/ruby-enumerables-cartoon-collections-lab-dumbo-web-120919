f9b9d4f746053a21b33f0e3c2e5fbf0fb86db5b3
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-05 18:52:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,9 +26,7 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  i = 0 
-  while i < strings.length do
-    if strings.include?(cheese_types[i])
+  if strings.include?.any?(cheese_types[i])
       return true 
     else
       i += 1 
