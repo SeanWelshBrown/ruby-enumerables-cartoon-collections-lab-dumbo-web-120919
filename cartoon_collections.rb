7938514c8bb0ced73cb16a4d0ced9ca01812b83a
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-05 16:39:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarf_names)
   dwarf_names.each_with_index do |object, index|
     number = index += 1 
     dwarf = object
-    puts indexdwarf
+    puts index + " " + dwarf
   end
 end
 
