a4191f02777dcbb0fc269c24e7189ac17b9a8997
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-05 19:01:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_the_cheese(strings)
   i = 0 
   while i < cheese_types.length do
     if strings.include?(cheese_types[i])
-      return true 
+      return 
     else
       i += 1 
     end
