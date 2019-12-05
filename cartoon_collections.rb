7cb8279c857b9a895e403a2d6982e0d71483c4d4
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-05 16:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,17 +7,15 @@ def roll_call_dwarves(dwarf_names)
 end
 
 
-
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |index|
+    index.
 end
-
 
 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
-
 
 
 def find_the_cheese# code an argument here
